a6454ef3a5c058a7aa49f98caf0ba2d9be375211
EdShel <eduardchelemetjev@gmail.com>
2021-03-06 16:08:04 +0200

    Made gaussian points generation

---
+++ ClusteringVisualisation/Extensions/NumbersExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public static class NumbersExtension
+    {
+        public static float Map01(this float value, float min, float max)
+        {
+            return (value - min) / (max - min);
+        }
+    }
+}
---
+++ ClusteringVisualisation/Extensions/RandomExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace System
+{
+    public static class RandomExtensions
+    {
+        public static double NextGaussian(this Random rng, double stdev)
+        {
+            double u1 = 1.0 - rng.NextDouble();
+            double u2 = 1.0 - rng.NextDouble();
+            double randStdNormal = Math.Sqrt(-2.0d * Math.Log(u1))
+                * Math.Sin(2.0 * Math.PI * u2);
+            return stdev * randStdNormal;
+        }
+    }
+}
